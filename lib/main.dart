27519461a2c5a4ffc88ffdc8871b345a72bd026d
Sergio Concha <ing.sergioconcha@gmail.com>
2024-03-20 17:42:56 -0600

    Este es la webView simple

--- lib/main.dart
+++ lib/main.dart
@@ -1,134 +1,30 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class YourWebView extends StatelessWidget {
-  String url;
-  YourWebView(this.url);
-
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter WebView example'),
-        ),
-        body: Builder(builder: (BuildContext context) {
-          return WebView(
-            initialUrl: url,
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller.complete(webViewController);
-            },
-            navigationDelegate: (NavigationRequest request) {
-              if (request.url.startsWith('https://www.youtube.com/')) {
-                print('blocking navigation to $request}');
-                return NavigationDecision.prevent;
-              }
-              print('allowing navigation to $request');
-              return NavigationDecision.navigate;
-            },
-            onPageStarted: (String url) {
-              print('Page started loading: $url');
-            },
-            onPageFinished: (String url) {
-              print('Page finished loading: $url');
-            },
-            gestureNavigationEnabled: true,
-          );
-        }));
-  }
-}
-
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<WebViewApp> createState() => _WebViewAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  youtube() {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => YourWebView('https://www.youtube.com/')));
-  }
-
+class _WebViewAppState extends State<WebViewApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Flutter WebView'),
       ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              IconButton(
-                onPressed: youtube,
-                tooltip: 'Open navigation youtube',
-                icon: const Icon(Icons.video_collection_rounded),
-              ),
-              IconButton(
-                onPressed: () {},
-                tooltip: 'Open navigation menu',
-                icon: const Icon(Icons.facebook),
-              ),
-              IconButton(
-                onPressed: () {},
-                tooltip: 'Open navigation menu',
-                icon: const Icon(Icons.telegram),
-              ),
-              IconButton(
-                onPressed: () {},
-                tooltip: 'Open navigation menu',
-                icon: const Icon(Icons.sd_card),
-              ),
-              IconButton(
-                onPressed: () {},
-                tooltip: 'Open navigation menu',
-                icon: const Icon(Icons.abc_outlined),
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
       ),
     );
   }
